482a52386d54046fa591e06c96b256ec4aab5944
Mynul Islam <mynul.sakil@gmail.com>
2023-06-10 21:37:16 +0600

    added animation

--- src/pages/Shared/SectionTittle.jsx
+++ src/pages/Shared/SectionTittle.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { useSpring, animated } from '@react-spring/web'
-const SectionTittle = ({heading,subHeading}) => {
+const SectionTittle = ({ heading, subHeading }) => {
     const [props, api] = useSpring(
         () => ({
-          from: { opacity: 0 },
-          to: { opacity: 1 },
+            from: { opacity: 0, fontSize: '4px' },
+            to: { opacity: 1, fontSize: '40px' },
+            delay:1000
         }),
         []
-      )
+    )
     return (
-        <animated.div style={props}>
         <div className='text-center my-10'>
-          <div className="divider "></div> 
-            {subHeading &&<p className='text-gray-500 mb-2 italic'>{subHeading}</p>}
-            <h2 className='text-rose-400 font-bold uppercase text-4xl'>{heading}</h2>
-            <div className="divider"></div> 
+            <div className="divider "></div>
+            {subHeading && <p className='text-gray-500 mb-2 italic'>{subHeading}</p>}
+            <animated.div style={props}>
+                <h2 className='text-rose-400 font-bold uppercase text-4xl'>{heading}</h2>
+            </animated.div>
+            <div className="divider"></div>
         </div>
-        </animated.div>
     );
 };
 
